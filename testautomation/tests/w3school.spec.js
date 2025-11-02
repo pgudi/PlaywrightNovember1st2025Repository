d74ef12531ed0b406225d42d34d0d7a624c89d4e
pgudi <prabhakargudi@gmail.com>
2025-11-02 08:27:28 +0530

    Launch and Navigate URL

---
+++ testautomation/tests/w3school.spec.js
@@ -0,0 +1,15 @@
+const {test, expect} = require('@playwright/test')
+
+test('Launch Browser and Navigate URL', async({page})=>{
+    await page.goto("https://www.w3schools.com/")
+    await page.waitForTimeout(4000)
+
+    const title = await page.title()
+    console.log("Title :"+title);
+    
+    const url = await page.url()
+    console.log("URL of Application :"+url);
+
+    
+    
+})
